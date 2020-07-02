cc03fa178e47c50ea9482b34b69ba436b80344b8
wuyaheng <wuyaheng2016@gmail.com>
2020-07-02 17:35:43 -0400

    add blob

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import Navbar from '../../components/Navbar/Navbar';
+import Goo from '../../components/Goo/Goo';
 
 class Home extends Component {
+    
     render() {
         return (
             <div>
             <Navbar />
-                home
+            <Goo />
             </div>
         )
     }
