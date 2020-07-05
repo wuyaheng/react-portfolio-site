941e048de63aabcde8af98342b9b3cff3f19fbf9
wuyaheng <wuyaheng2016@gmail.com>
2020-07-05 00:15:27 -0400

    add project title text animation

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './Project.module.css';
+import 'animate.css';
 
 function Project(props) {
         return (
@@ -10,8 +11,8 @@ function Project(props) {
                     </div>
                     
                     <div className="card-body text-center">
-                      <h5 className={styles.cardtitle}>{props.name}</h5> 
-                      <p className="card-text">{props.description}</p> 
+                      <h5 className={styles.cardtitle} class="animate__animated animate__zoomIn">{props.name}</h5> 
+                      <p className="card-text animate__animated animate__zoomIn animate__delay-1s">{props.description}</p> 
                       <div className="row m-0">
                     <div className="col-6 m-0">
                       <a href={props.github} id={styles.learnMoreBtn} className="btn waves-effect waves-light white-text">GitHub</a>
